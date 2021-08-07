9b75055ea10b9b4805a2562670a528a0d8f2bb10
Mahmoud Fouad Ali Abo Ali <mfaworkstation@gmail.com>
2021-08-07 22:32:51 +0200

    commit

--- website/app.js
+++ website/app.js
@@ -4,3 +4,5 @@
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 const api = '566c9ff97bfc96be30227785b9abadb2';
+const urlZip = 'api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}';
+
