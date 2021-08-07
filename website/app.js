e592b71f0e0c85558426ba881afdeae09e64e4cb
Mahmoud Fouad Ali Abo Ali <mfaworkstation@gmail.com>
2021-08-07 23:02:40 +0200

    commit

--- website/app.js
+++ website/app.js
@@ -4,14 +4,14 @@
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 // my API from OpenWeatherMap.com
-const api = '566c9ff97bfc96be30227785b9abadb2';
+const APIkey = '566c9ff97bfc96be30227785b9abadb2';
 // set full url from OpenWeatherMap.com
 // get the generate button by id
 const genButton = document.getElementById('generate');
 
 genButton.addEventListener('click', () => {
-    const zipId = document.getElementById('zip').value;
-    const urlZip = `api.openweathermap.org/data/2.5/weather?zip={zip code}&appid={API key}&units=imperial`;
+    const zipCode = document.getElementById('zip').value;
+    const urlZip = `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${APIkey}&units=imperial`;
     let res = fetch(urlZip);
     // test res
     console.log(res); 
