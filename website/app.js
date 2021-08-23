54a12e3720b84065d4979eed067dc2126c60ae85
Mahmoud Fouad Ali Abo Ali <mfaworkstation@gmail.com>
2021-08-23 16:54:18 +0200

    commit

--- website/app.js
+++ website/app.js
@@ -62,7 +62,7 @@ const zippy = async () => {
                     tempId.innerHTML = `temp : ${data.temp}`;
                     contentId.innerHTML = `feeling : ${data.content}`;
                 })
-        } catch (error) { console.log('error',error); }
+        } catch (error) { console.log(error); }
     }
     // call the function
     updateUI();
